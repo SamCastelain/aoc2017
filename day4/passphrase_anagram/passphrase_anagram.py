3e1d4175ed61e176223c65d7f8816003df9d2dab
Sam Castelain <sam.castelain@zenso.be>
2017-12-07 08:40:29 +0100

    Add solution for day4

---
+++ day4/passphrase_anagram/passphrase_anagram.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.6
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(
+        description='Count the valid passphrases for AoC 2017 day 4')
+parser.add_argument('file', metavar='file', type=str, nargs=1)
+
+args = parser.parse_args()
+
+valid_phrases = 0
+with open(args.file[0]) as f:
+    for line in f:
+        words = line.split()
+        anagrams_set = {''.join(sorted(word)) for word in words}
+        if len(anagrams_set) == len(words):
+            valid_phrases += 1
+
+print(valid_phrases)
